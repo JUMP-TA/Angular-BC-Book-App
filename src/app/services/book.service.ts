e928a50f454d03c0cf3b87aa3bcb1e3b6f160352
Orquidia Moreno <orquidiamoreno26@gmail.com>
2024-06-12 10:46:01 -0400

    first commit

---
+++ src/app/services/book.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Book } from '../models/book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  private url: string = 'http://localhost:3000/book';
+
+  constructor(private http:HttpClient) { }
+
+  getBooks() {
+    return this.http.get<Book[]>( this.url );
+  }
+
+  getBookById(id:string) {
+    return this.http.get<Book>( this.url + "/" + id );
+  }
+
+  addBook(newBook:any) {
+    return this.http.post(this.url, newBook);
+  }
+
+  updateBook(updatedBook:Book) {
+    return this.http.put(this.url + "/" + updatedBook.id, updatedBook);
+  }
+
+  deleteBook(id: string) {
+    return this.http.delete(this.url + "/" + id);
+  }
+  
+}
